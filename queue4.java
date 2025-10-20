441d6b2e744bfec415283b7edabb37dc004c4c88
Rohanmpatil16 <rohanmpatil18@gmail.com>
2025-10-20 21:02:32 +0530

    added program of queue using two stacks

---
+++ queue4.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class queue4
+        {
+    
+       static Stack<Integer>s1=new Stack<>();
+       static Stack<Integer>s2=new Stack<>();
+        
+
+        public static boolean isEmpty()
+        {
+            return s1.isEmpty();
+        }
+
+        public static void add(int data)
+        {
+            while(!s1.isEmpty())
+            {
+                s2.push(s1.pop());
+            }
+
+            s1.push(data);
+
+            while(!s2.isEmpty())
+            {
+                s1.push(s2.pop());
+            }
+        }
+        public static int remove()
+        {
+            if(isEmpty())
+            {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            else
+            {
+                return s1.pop();
+            }
+        }
+        public static int peek()
+        {
+            if(isEmpty())
+            {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            else
+            {
+                return s1.peek();
+            }
+        }
+    public static void main(String args[])
+    {
+        queue4 q=new queue4();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        System.out.println("Removed element: " + q.remove());
+        System.out.println("Peek element: " + q.peek());
+    }
+}
